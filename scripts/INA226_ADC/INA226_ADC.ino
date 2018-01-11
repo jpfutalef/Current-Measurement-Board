a3a00b8889cc7d4457e180486500b759af99154f
francisco <femolina@uc.cl>
2018-01-11 12:53:02 -0300

    INA226 script

---
+++ scripts/INA226_ADC/INA226_ADC.ino
@@ -0,0 +1,7 @@
+/*
+This scripts allows usage of INA226's shunt resistor voltage measurement ADC for any
+high resolution voltage measurement application thanks to its high bit range (16 bit),
+from -81.9175 to 81.92 mV fixed (according to datasheet).
+
+It is assumed that INA226 doesn't have any shunt resistor connected to IN+ and IN- inputs.
+*/
