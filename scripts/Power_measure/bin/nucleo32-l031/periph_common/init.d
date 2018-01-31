b82bb80e87ec224506f41c7605c81c388899c83e
jpfutalef <jpfutalef@gmail.com>
2018-01-31 16:36:47 -0300

    update folder

--- scripts/Power_measure/bin/nucleo32-l031/periph_common/init.d
+++
@@ -1,5 +0,0 @@
-/home/inria/Documentos/Current-Measurement-Board/scripts/Power_measure/bin/nucleo32-l031/periph_common/init.o: \
- /home/inria/Documentos/Current-Measurement-Board/RIOT/drivers/periph_common/init.c \
- /home/inria/Documentos/Current-Measurement-Board/scripts/Power_measure/bin/nucleo32-l031/riotbuild/riotbuild.h
-
-/home/inria/Documentos/Current-Measurement-Board/scripts/Power_measure/bin/nucleo32-l031/riotbuild/riotbuild.h:
